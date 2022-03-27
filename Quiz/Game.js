6e23ba01d1e00760d2ebfd8b1bb7531dce5c87ab
Jellyoo <elliot7777@outlook.com>
2022-03-27 04:10:44 +0100

    Signed-off-by: Jellyoo <elliot7777@outlook.com>

--- Quiz/Game.js
+++ Quiz/Game.js
@@ -75,7 +75,7 @@ startGame = () => {
     getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {           /* If questions finished go to the final end hyml page */
         localStorage.setItem("mostRecentScore", score);
-        return window.location.assign("End.html");                                     /* take to End html page */
+        return window.location.assign('End.html');                                     /* take to End html page */
     }
     
     
